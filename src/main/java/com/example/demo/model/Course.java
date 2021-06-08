11cab0a346c34aeae9d91913472606ff99d8bcc0
sandu.nicula <sandu.nicula@gmail.com>
2021-06-08 17:55:47 +0300

    feat: added liquibase and created enities

---
+++ src/main/java/com/example/demo/model/Course.java
@@ -0,0 +1,59 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+    @Column(name = "id", unique = true, nullable = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "credits", nullable = false)
+    private Long credits;
+
+    @Column(name = "teacher_name", nullable = false)
+    private String teacherName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getCredits() {
+        return credits;
+    }
+
+    public void setCredits(Long credits) {
+        this.credits = credits;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+}
